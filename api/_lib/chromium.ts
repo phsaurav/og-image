b983741975be42e58ed0e3b7c16ea5f23449fc4f
PH Saurav <phsaurav29@gmail.com>
2022-06-21 21:31:32 +0600

    Fixed

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,21 +1,24 @@
-import core from 'puppeteer-core';
-import { getOptions } from './options';
-import { FileType } from './types';
+import core from "puppeteer-core";
+import { getOptions } from "./options";
+import { FileType } from "./types";
 let _page: core.Page | null;
 
-async function getPage(text: string,isDev: boolean) {
-    if (_page) {
-        return _page;
-    }
-    const options = await getOptions(isDev);
-    const browser = await core.launch(options);
-    _page = await browser.newPage(`https://tutor-media.liilab.com/tutor/home/og/${text}`);
-    return _page;
+async function getPage(isDev: boolean) {
+	if (_page) {
+		return _page;
+	}
+	const options = await getOptions(isDev);
+	const browser = await core.launch(options);
+	_page = await browser.newPage();
+	return _page;
 }
 
 export async function getScreenshot(text: string, type: FileType, isDev: boolean) {
-    const page = await getPage( text,isDev);
-    await page.setViewport({ width: 1200, height: 630 });
-    const file = await page.screenshot({ type });
-    return file;
+	const page = await getPage(isDev);
+	await page.setViewport({ width: 1200, height: 630 });
+	await page.goto(`https://tutor-media.liilab.com/tutor/home/og/${text}`, {
+		waitUntil: "networkidle2",
+	});
+	const file = await page.screenshot({ type });
+	return file;
 }
