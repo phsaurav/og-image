e9e53581311da0949fed100cac0cc98a63045511
PH Saurav <phsaurav29@gmail.com>
2022-06-27 13:29:02 +0600

    Any Fix

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -3,5 +3,5 @@ export type FileType = "png" | "jpeg";
 export interface ParsedRequest {
 	text: string;
 	gender?: string | string[] | undefined;
-	studentInfo?: string | string[] | undefined;
+	studentInfo?: any;
 }
