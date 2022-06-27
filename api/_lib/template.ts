2d128d845b3c65a5cdeb41f8b5e89351d277c07b
PH Saurav <phsaurav29@gmail.com>
2022-06-27 12:24:59 +0600

    Twemoji removed

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import { sanitizeHtml } from "./sanitizer";
 import { ParsedRequest } from "./types";
-const twemoji = require("twemoji");
+// const twemoji = require("twemoji");
 // const twOptions = { folder: "svg", ext: ".svg" };
 // const emojify = (text: string) => twemoji.parse(text, twOptions);
 
